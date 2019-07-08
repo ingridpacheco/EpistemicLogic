f050a3641aed17ea642cd3be8cd546ee336a4de6
ingridpacheco <iq.pacheco23@gmail.com>
2019-07-08 17:00:35 -0300

    Updates

---
+++ precache-manifest.0fa81542c56a79f2764d8baf59b5fe97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b6282043985a5ec07ea",
+    "url": "/epistemic-logic/static/css/main.c0846c05.chunk.css"
+  },
+  {
+    "revision": "9b6282043985a5ec07ea",
+    "url": "/epistemic-logic/static/js/main.80aa7564.chunk.js"
+  },
+  {
+    "revision": "57d0c510a826e4887f76",
+    "url": "/epistemic-logic/static/js/runtime~main.841cfc7e.js"
+  },
+  {
+    "revision": "7c3c485b5cbf5e7bc406",
+    "url": "/epistemic-logic/static/js/2.44015def.chunk.js"
+  },
+  {
+    "revision": "be70d7dd4747afad93cef1636ef707c0",
+    "url": "/epistemic-logic/static/media/bg-index.be70d7dd.jpg"
+  },
+  {
+    "revision": "bdd94ea37c35af72c711ffd50f04347d",
+    "url": "/epistemic-logic/static/media/graph.bdd94ea3.png"
+  },
+  {
+    "revision": "a1a82e977c8f69a2185c0f582e4fd0e4",
+    "url": "/epistemic-logic/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/epistemic-logic/precache-manifest.0fa81542c56a79f2764d8baf59b5fe97.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/epistemic-logic/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
